bfa13cfa4e06b944c8f30ed0775eeb53237c55d3
duongdev <=>
2020-12-22 08:54:32 +0700

    Fix active storage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @rooms = Room.includes(:category)
+                 .includes(:room_pictures)
                  .top_hired.limit Settings.model.limit.top_hired_home_page
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
   has_many :rooms, dependent: :destroy
 
   def rooms_each_category
-    rooms.top_hired.limit Settings.model.limit.rooms_per_cate
+    rooms.includes(:room_pictures)
+         .top_hired
+         .limit Settings.model.limit.rooms_per_cate
   end
 end
